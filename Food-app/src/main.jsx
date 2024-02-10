836f9eeefd5879dfc433dbae93ffcfda54a13106
Nicolai Sørensen <nicolai3030@gmail.com>
2024-02-10 16:24:05 +0100

    added pages directory with home page and recipe page

--- Food-app/src/main.jsx
+++ Food-app/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// "surroinding out apllication with browserRouter -> react-router-dom to add pages"
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router> 
     <App />
-  </React.StrictMode>,
-)
+  </Router>
+);
